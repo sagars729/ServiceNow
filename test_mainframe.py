f0c615dc0d8733cb86497785b7b5379ae9b800b9
sagars729 <ssaxena1@terpmail.umd.edu>
2020-02-28 17:00:10 -0500

    Working Tests

--- test_mainframe.py
+++ test_mainframe.py
@@ -9,61 +9,70 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 
 import os
-os.environ["PYTHONPATH"] = "."
+#os.environ["PYTHONPATH"] = "."
+os.system("export PYTHONPATH=\".\"")
 
+import pytest
 from mainframe import Mainframe
-from getpass import getpass
 
 options = webdriver.ChromeOptions()
 options.add_argument("--disable-popup-blocking")
 chrome_driver_path = os.path.join(".", "chromedriver.exe")
-driver = webdriver.Chrome(options=options)#executable_path=chrome_driver_path)
-mf = Mainframe(driver)
+
+def init():
+    driver = webdriver.Chrome(options=options)#executable_path=chrome_driver_path)
+    mf = Mainframe(driver)
+    mf.side_door_login()
+    mf.impersonate("Jess Jacobson")
+    return mf
 
 def test_sanity():
-    print(input("Echo: "))
-    getpass("Password: ")
-    mf.login()
-    mf.impersonate("Jess Jacobson")
+    assert(1==1)#mf.impersonate("Jess Jacobson")
 
-
-#def test_required_fields_c1():
-#    mf.navigate_to_form()
-#    mf.enter_manager("Scott Gibson")
-#    mf.select_environment("Development")
-#    mf.select_application("ADM", "this is a reason to request access", typ="student")
-#    mf.select_application("FIN", typ="student")
-#    mf.submit_form(False)
-#    ticket, request = mf.check_submit()
-#    assert(ticket != None)    
+def test_required_fields_c1():
+    mf = init()
+    mf.navigate_to_form()
+    mf.enter_manager("Scott Gibson")
+    mf.select_environment("Development")
+    mf.select_application("ADM", "this is a reason to request access", typ="student")
+    mf.select_application("FIN", typ="student")
+    mf.submit_form(False)
+    ticket, request = mf.check_submit()
+    assert(ticket != None)
+    mf.driver.quit()
 
 def test_required_fields_c2():
-	mf.navigate_to_form()
-	mf.select_environment("Development")
-	mf.select_application("ADM", "this is a reason to request access", typ="student")
-	mf.select_application("FIN", typ="student")
-	mf.submit_form(False)
-	ticket, request = mf.check_submit()
-	#mf.leave_form() 
-	assert(ticket == None)    
+    mf = init()
+    mf.navigate_to_form()
+    mf.select_environment("Development")
+    mf.select_application("ADM", "this is a reason to request access", typ="student")
+    mf.select_application("FIN", typ="student")
+    mf.submit_form(False)
+    ticket, request = mf.check_submit()
+    assert(ticket == None)    
+    mf.driver.quit()
 
 def test_required_fields_c4():
-	mf.navigate_to_form()
-	mf.enter_manager("Scott Gibson")
-	mf.select_application("ADM", "this is a reason to request access", typ="student")
-	mf.select_application("FIN", typ="student")
-	mf.submit_form(False)
-	ticket, request = mf.check_submit()
-	#mf.leave_form() 
-	assert(ticket == None)    
+    mf = init()
+    mf.navigate_to_form()
+    mf.enter_manager("Scott Gibson")
+    mf.select_application("ADM", "this is a reason to request access", typ="student")
+    mf.select_application("FIN", typ="student")
+    mf.submit_form(False)
+    ticket, request = mf.check_submit()
+    #mf.leave_form() 
+    assert(ticket == None)    
+    mf.driver.quit()
 
 def test_required_fields_c5():
-	mf.navigate_to_form()
-	mf.enter_manager("Scott Gibson")
-	mf.select_environment("Development")
-	mf.select_application("ADM", typ="student")
-	mf.select_application("FIN", typ="student")
-	mf.submit_form(False)
-	ticket, request = mf.check_submit()
-	#mf.leave_form() 
-	assert(ticket == None)    
+    mf = init()
+    mf.navigate_to_form()
+    mf.enter_manager("Scott Gibson")
+    mf.select_environment("Development")
+    mf.select_application("ADM", typ="student")
+    mf.select_application("FIN", typ="student")
+    mf.submit_form(False)
+    ticket, request = mf.check_submit()
+    #mf.leave_form() 
+    assert(ticket == None)    
+    mf.driver.quit()
